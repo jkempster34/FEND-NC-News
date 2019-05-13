7264d1d6f60733349f661f64ededc6b0201c48a4
Joseph Kempster <jkempster34@gmail.com>
2019-05-13 19:20:51 +0100

    user can log-in, working on global log-in popup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import React, { Component } from "react";
+import { Router, Link } from "@reach/router";
 import "./App.css";
-import Header from "./components/Header";
+import Home from "./pages/Home";
+import Articles from "./pages/Articles.jsx";
+import Header from "./components/Header.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
 class App extends Component {
+  state = {
+    loggedInUser: ""
+  };
   render() {
     return (
       <div className="App">
-        <Header />
-        <p>Home</p>
+        <Header loginUser={this.loginUser} />
+        <Router>
+          <Home path="/" />
+          <Articles path="/articles" />
+          <NotFound default />
+        </Router>
       </div>
     );
   }
+  loginUser = username => {
+    this.setState({ loggedInUser: username });
+  };
 }
 
 export default App;
--- src/api.js
+++ src/api.js
@@ -1,10 +1,14 @@
 import Axios from "axios";
 const url = " https://nc-news-joseph-kempster.herokuapp.com/api";
 
-const getArticles = () => {
+export const getArticles = () => {
   return Axios.get(`${url}/articles`).then(({ data: { articles } }) => {
     return articles;
   });
 };
 
-export default getArticles;
+export const getUser = username => {
+  return Axios.get(`${url}/users/${username}`).then(({ data: { user } }) => {
+    return user;
+  });
+};
