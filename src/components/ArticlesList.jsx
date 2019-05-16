6d756a1bbc30679dfd1ff05fe757a7d4a79677c2
Joseph Kempster <josephkempster@Josephs-MacBook-Pro.local>
2019-05-16 17:05:50 +0100

    added votes to all articles

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import { Link } from "@reach/router";
 import { changeTimeToAgo } from "../utils/changeTimeToAgo.js";
+import VoteButtons from "../components/VoteButtons.jsx";
 
 const ArticlesList = props => {
   const { articles, loading } = props;
+
   return loading ? (
     <p>loading...</p>
   ) : (
     <ul>
       {articles.map(article => {
+        console.log(article);
         return (
           <li key={article.article_id}>
             <Link to={`/articles/${article.article_id}`}>{article.title}</Link>
             COMMENTS: {article.comment_count}
             AUTHOR : {article.author}
             CREATED_AT: {changeTimeToAgo(article.created_at)}
-            VOTES: {article.votes}
+            <VoteButtons
+              articleId={article.article_id}
+              votes={article.votes}
+              type={"article"}
+            />
           </li>
         );
       })}
