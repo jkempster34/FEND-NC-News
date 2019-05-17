1d3447b60992ae3a91d5a6e6a90cce9a5291c22c
Joseph Kempster <josephkempster@Josephs-MacBook-Pro.local>
2019-05-17 15:30:19 +0100

    started styling

--- src/components/Header.LoginPopup.jsx
+++ src/components/Header.LoginPopup.jsx
@@ -9,11 +9,19 @@ class LoginPopup extends Component {
   render() {
     return (
       <div className="login-popup">
-        <div className="login-popup-inner">
+        <div className="login-popup-inner" onKeyDown={this.handleEsc}>
+          <div className="login-popup-header">
+            <h1 id="login-title">LOGIN</h1>
+            <button
+              onClick={this.props.toggleLoginPopup}
+              id="header-close-button"
+            />
+          </div>
           <form onSubmit={this.handleSubmit}>
             <label>
               Username:{" "}
               <input
+                autoFocus
                 onChange={this.handleInput}
                 type="text"
                 placeholder="example: jessjelly"
@@ -21,12 +29,15 @@ class LoginPopup extends Component {
             </label>
             <button>SIGN IN</button>
           </form>
-          <button onClick={this.props.toggleLoginPopup}>close</button>
+
           {this.state.wrongLogin && <p>No user exists!</p>}
         </div>
       </div>
     );
   }
+  handleEsc = event => {
+    if (event.keyCode === 27) this.props.toggleLoginPopup();
+  };
 
   handleInput = event => {
     this.setState({ userNameInput: event.target.value });
@@ -37,6 +48,8 @@ class LoginPopup extends Component {
     getUser(this.state.userNameInput)
       .then(validUser => {
         this.props.loginUser(validUser);
+      })
+      .then(() => {
         this.props.toggleLoginPopup();
       })
       .catch(() => this.setState({ wrongLogin: true }));
