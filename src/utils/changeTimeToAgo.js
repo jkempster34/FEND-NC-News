fea2f7509bf8db3fb81a45e3bb8cf33514212381
Joseph Kempster <josephkempster@Josephs-MacBook-Pro.local>
2019-05-17 09:27:48 +0100

    updated changetimetoago util function

--- src/utils/changeTimeToAgo.js
+++ src/utils/changeTimeToAgo.js
@@ -10,16 +10,40 @@ export const changeTimeToAgo = datetime => {
   const msPerYear = msPerDay * 365;
 
   if (elapsed < msPerMinute) {
-    return Math.round(elapsed / 1000) + " seconds ago";
+    return (
+      "approximately " +
+      Math.round(elapsed / 1000) +
+      (Math.round(elapsed / 1000) === 1 ? " second ago" : " seconds ago")
+    );
   } else if (elapsed < msPerHour) {
-    return Math.round(elapsed / msPerMinute) + " minutes ago";
+    return (
+      "approximately " +
+      Math.round(elapsed / msPerMinute) +
+      (Math.round(elapsed / msPerMinute) === 1 ? " minute ago" : " minutes ago")
+    );
   } else if (elapsed < msPerDay) {
-    return Math.round(elapsed / msPerHour) + " hours ago";
+    return (
+      "approximately " +
+      Math.round(elapsed / msPerHour) +
+      (Math.round(elapsed / msPerHour) === 1 ? " hour ago" : " hours ago")
+    );
   } else if (elapsed < msPerMonth) {
-    return "approximately " + Math.round(elapsed / msPerDay) + " days ago";
+    return (
+      "approximately " +
+      Math.round(elapsed / msPerDay) +
+      (Math.round(elapsed / msPerDay) === 1 ? " day ago" : " days ago")
+    );
   } else if (elapsed < msPerYear) {
-    return "approximately " + Math.round(elapsed / msPerMonth) + " months ago";
+    return (
+      "approximately " +
+      Math.round(elapsed / msPerMonth) +
+      (Math.round(elapsed / msPerMonth) === 1 ? " month ago" : " months ago")
+    );
   } else {
-    return "approximately " + Math.round(elapsed / msPerYear) + " years ago";
+    return (
+      "approximately " +
+      Math.round(elapsed / msPerYear) +
+      (Math.round(elapsed / msPerYear) === 1 ? " year ago" : " years ago")
+    );
   }
 };
